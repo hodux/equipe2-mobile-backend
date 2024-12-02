8a0214792486cdf9dd48934fe769048d2ca15173
Yves-Shaheem <shaheemshedid4@gmail.com>
2024-12-02 22:27:58 +0000

    crud for users

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# mobile-backend
+# Mobile-Backend
 Épreuve Finale - Développement d'une Application Android 
+### Étapes
+* Exécuter le script " scripts.sql " avec la commande  " mysql -u root -p < scripts.sql "  
---
+++ src/queries.js
@@ -0,0 +1,60 @@
+import mysql from 'mysql2';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const pool = mysql.createPool({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
+}).promise()
+
+export async function createUser(username, email, password){
+    //DEBUG
+    console.log(`Queries.js : creating user with email: ${email}, username: ${username} and password : ${password}`)
+    //QUERY
+    const query = await pool.query(`INSERT INTO users (username,email,password) VALUES (?,?,?);`,[username,email,password])
+    const [rows] = await pool.query(`SELECT user_id, username, email FROM users WHERE username=? and email=?`,[username,email])
+    return rows[0]
+}
+//TEST
+//console.log(await createUser("bob","bob@bob.bob", "bob"));
+
+export async function getUserById(id){
+    //DEBUG
+    console.log(`Queries.js : retriving user with id: ${id}`)
+    //QUERY
+    const [rows] = await pool.query('SELECT user_id, username, email FROM users where user_id=?', [id]);
+    return rows[0];
+}
+//TEST
+//console.log(await getUserById(1))
+
+export async function login(usernameEmail, password){
+    //DEBUG
+    console.log(`Queries.js : authentificate with username or email: ${usernameEmail}`)
+    //QUERY
+    const [user] = await pool.query(`SELECT * FROM users WHERE (username=? OR email=?) AND password=?;`,[usernameEmail,usernameEmail,password])
+    return user[0];
+}
+//TEST
+//console.log(await login(bob, bob));
+export async function updateUser(username, email, id){
+    //DEBUG
+    console.log(`Queries.js : update users with userData.id : ${id}`)
+    //QUERY
+    const [rows] = await pool.query(`UPDATE users SET username = ?, email = ? WHERE user_id = ?;`,[username,email,id])
+    return true
+}
+//TEST
+//console.log(await updateUser(toto, toto@gmail.com, 1));
+export async function deleteUserById(id){
+    //DEBUG
+    console.log(`Database : delete users with id : ${id}`)
+    //
+    const status = await pool.query(`DELETE FROM users WHERE user_id = ?;`,[id])
+    return status[0].affectedRows;
+}
+//TEST
+// console.log(await deleteUserById(1));
